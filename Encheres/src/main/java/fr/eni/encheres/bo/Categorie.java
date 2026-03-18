769ea71148fb182e08752bfb687ce40d7a60fa7a
mmmmaaaax <maxime.bourret2026@campus-eni.fr>
2026-03-18 16:41:11 +0100

    [Ajout]objet metier categories et enchere

--- Encheres/src/main/java/fr/eni/encheres/bo/Categorie.java
+++ Encheres/src/main/java/fr/eni/encheres/bo/Categorie.java
@@ -1,6 +1,44 @@
 package fr.eni.encheres.bo;
 
 
+import java.util.List;
+
 public class Categorie {
 
+    private Long noCategorie;
+    private String libelle;
+    private List<Article> articles;
+
+    public Categorie() {
+    }
+
+    public Categorie(Long noCategorie, String libelle, List<Article> articles) {
+        this.noCategorie = noCategorie;
+        this.libelle = libelle;
+        this.articles = articles;
+    }
+
+    public Long getNoCategorie() {
+        return noCategorie;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    public void setNoCategorie(Long noCategorie) {
+        this.noCategorie = noCategorie;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 }
--- Encheres/src/main/java/fr/eni/encheres/bo/Enchere.java
+++ Encheres/src/main/java/fr/eni/encheres/bo/Enchere.java
@@ -1,8 +1,55 @@
 package fr.eni.encheres.bo;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 
 public class Enchere implements Serializable {
 
+        private LocalDateTime dateEnchere;
+        private int montant_enchere;
+        Utilisateur utilisateur;
+        Article article;
+
+    public Enchere() {
+    }
+
+    public Enchere(LocalDateTime dateEnchere, Article article, Utilisateur utilisateur, int montant_enchere) {
+        this.dateEnchere = dateEnchere;
+        this.article = article;
+        this.utilisateur = utilisateur;
+        this.montant_enchere = montant_enchere;
+    }
+
+    public LocalDateTime getDateEnchere() {
+        return dateEnchere;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public Utilisateur getUtilisateur() {
+        return utilisateur;
+    }
+
+    public int getMontant_enchere() {
+        return montant_enchere;
+    }
+
+    public void setDateEnchere(LocalDateTime dateEnchere) {
+        this.dateEnchere = dateEnchere;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public void setMontant_enchere(int montant_enchere) {
+        this.montant_enchere = montant_enchere;
+    }
+
+    public void setUtilisateur(Utilisateur utilisateur) {
+        this.utilisateur = utilisateur;
+    }
 }
