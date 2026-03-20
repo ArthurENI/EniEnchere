0ec51fb47d9cafe86dd094a5e419ceba01eb6941
Thomas TANNEAU <ttanneau2018@campus-eni.fr>
2026-03-20 12:01:22 +0100

    [FIX] Session utilisateur page encheres

--- Encheres/src/main/java/fr/eni/encheres/controller/ArticleController.java
+++ Encheres/src/main/java/fr/eni/encheres/controller/ArticleController.java
@@ -5,14 +5,12 @@ import fr.eni.encheres.services.ArticleService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
 
+@SessionAttributes({"loggedUser"})
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
