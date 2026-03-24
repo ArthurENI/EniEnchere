d39c8d85404ec1b8685192498cb5278ab4a63261
mmmmaaaax <maxime.bourret2026@campus-eni.fr>
2026-03-24 13:49:05 +0100

    Fix merge

--- Encheres/src/main/java/fr/eni/encheres/controller/EnchereController.java
+++ Encheres/src/main/java/fr/eni/encheres/controller/EnchereController.java
@@ -1,14 +1,14 @@
 package fr.eni.encheres.controller;
 
+import fr.eni.encheres.bo.Article;
 import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.services.ArticleService;
 import fr.eni.encheres.services.EnchereService;
+import fr.eni.encheres.services.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +16,13 @@ import java.util.List;
 @RequestMapping("/encheres")
 public class EnchereController {
     EnchereService enchereService;
+    ArticleService articleService;
+    UtilisateurService utilisateurService;
 
-    public EnchereController(EnchereService enchereService) {
+    public EnchereController(EnchereService enchereService, ArticleService articleService, UtilisateurService utilisateurService) {
         this.enchereService = enchereService;
+        this.articleService = articleService;
+        this.utilisateurService = utilisateurService;
     }
 
     @GetMapping("/liste")
@@ -30,14 +34,17 @@ public class EnchereController {
         return "encheres/encheres-page";
     }
 
-    @GetMapping("/details")
+    @GetMapping("/details/{id}")
     public String detailEnchere(
-            @RequestParam(name = "id", required = true) Long id,
+            @PathVariable(name = "id") Long id,
             Model model) {
         if (id > 0) {
             Enchere enchere = enchereService.selectEnchereById(id);
+
             if (enchere != null) {
+
                 model.addAttribute("enchere", enchere);
+
                 return "encheres/enchere-detail-page";
             }
         }
--- Encheres/src/main/java/fr/eni/encheres/dao/jdbc/DAOEnchereImpl.java
+++ Encheres/src/main/java/fr/eni/encheres/dao/jdbc/DAOEnchereImpl.java
@@ -1,8 +1,11 @@
 package fr.eni.encheres.dao.jdbc;
 
+import fr.eni.encheres.bo.Article;
 import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dao.IDAOEnchere;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -15,50 +18,56 @@ public class DAOEnchereImpl implements IDAOEnchere {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    private final RowMapper<Enchere> enchereRowMapper = (rs, rowNum) -> {
+        Enchere ench = new Enchere();
+        ench.setNoEnchere(rs.getLong("no_enchere"));
+        ench.setDateEnchere(rs.getTimestamp("date_enchere").toLocalDateTime());
+        ench.setMontant_enchere(rs.getInt("montant_enchere"));
+        Article arti = new Article();
+        arti.setNoArticle(rs.getLong("no_article"));
+        arti.setNomArticle(rs.getString("nom_article"));
+        arti.setMiseAPrix(rs.getInt("prix_initial"));
+        Utilisateur user = new Utilisateur();
+        user.setNoUtilisateur(rs.getLong("no_utilisateur"));
+        user.setPseudo(rs.getString("pseudo"));
+        ench.setUtilisateur(user);
+        ench.setArticle(arti);
+        return ench;
+    };
+
+
     @Override
     public Enchere selectById(Long id) {
-        String sql = "SELECT * FROM encheres WHERE no_enchere = ?";
-
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
-            Enchere ench = new Enchere();
-            ench.setNoEnchere(rs.getLong("no_enchere"));
-            ench.setDateEnchere(rs.getTimestamp("date_enchere").toLocalDateTime());
-            ench.setMontant_enchere(rs.getInt("montant_enchere"));
-
-            return ench;
-            }, id);
+        String sql = " SELECT e.*, a.nom_article, a.prix_initial, u.pseudo" +
+                "            FROM encheres e" +
+                "            JOIN articles a ON e.no_article = a.no_article" +
+                "            JOIN utilisateurs u ON e.no_utilisateur = u.no_utilisateur" +
+                "            WHERE e.no_enchere = ?";
+        return jdbcTemplate.queryForObject(sql, enchereRowMapper, id);
     }
 
     @Override
     public List<Enchere> findAll() {
-        String sql = "SELECT * FROM encheres";
-
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Enchere ench = new Enchere();
-            ench.setNoEnchere(rs.getLong("no_enchere"));
-            ench.setDateEnchere(rs.getTimestamp("date_enchere").toLocalDateTime());
-            ench.setMontant_enchere(rs.getInt("montant_enchere"));
-
-            return ench;
-        });
+        String sql = "SELECT e.*, a.nom_article, a.prix_initial, u.pseudo" +
+                "            FROM encheres e" +
+                "            JOIN articles a ON e.no_article = a.no_article" +
+                "            JOIN utilisateurs u ON e.no_utilisateur = u.no_utilisateur";
+        return jdbcTemplate.query(sql, enchereRowMapper);
     }
 
     @Override
     public List<Enchere> findByArticle(Long noArticle) {
-        String sql = "SELECT * FROM encheres WHERE no_article = ?";
-
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Enchere ench = new Enchere();
-            ench.setNoEnchere(rs.getLong("no_enchere"));
-            ench.setDateEnchere(rs.getTimestamp("date_enchere").toLocalDateTime());
-            ench.setMontant_enchere(rs.getInt("montant_enchere"));
-            return ench;
-            }, noArticle);
+        String sql = "SELECT e.*, a.nom_article, a.prix_initial, u.pseudo" +
+                "            FROM encheres e" +
+                "            JOIN articles a ON e.no_article = a.no_article" +
+                "            JOIN utilisateurs u ON e.no_utilisateur = u.no_utilisateur" +
+                "            WHERE e.no_article = ?";
+        return jdbcTemplate.query(sql, enchereRowMapper, noArticle);
     }
 
     @Override
     public Enchere create(Enchere enchere) {
-        String sql = "INSERT INTO encheres (date_enchere, montant_enchere, no_article, no_utilisateur) VALUES (?, ?)";
+        String sql = "INSERT INTO encheres (date_enchere, montant_enchere, no_article, no_utilisateur) VALUES (?, ?, ?,?)";
         jdbcTemplate.update(sql,
                 enchere.getDateEnchere(),
                 enchere.getMontant_enchere(),
